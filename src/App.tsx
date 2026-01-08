a1d966d55fce4b49cd1991eb3b86d47007be0cbe
emonmahdi <developeremon1997@gmail.com>
2026-01-08 12:36:13 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+ 
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+       <h2 className='text-3xl bg-purple-700 font-bold p-4'>Todo App redux toolkit</h2>
+    </>
+  )
+}
+
+export default App
